d83d939d5953cd90912f6f3dced74a58afa59c0a
hkumar062 <55359425+hkumar062@users.noreply.github.com>
2019-09-18 16:24:25 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -21,34 +21,41 @@ server = function(input, output) {
     affluence <- affluence[order(affluence$Proportion),]
   })
   
+  mall <- reactive({
+    
+    mall <- data.frame(section = c("#1 Retail", "#2 Services", "#3 Restaurant", "#4 Foods"), 
+                       Proportion = c(200, 175, 150, 50))
+    
+    
+    
+  })
+  
   mall1 <- reactive({
-        
-        mall1 <- data.frame(section = c("#1 Retail", "#2 Services", "#3 Restaurant", "#4 Foods"), 
-                           Proportion = c(200, 175, 150, 50))
-        
-        
-        
-    })
     
-    mall2 <- reactive({
-        
-        mall2 <- data.frame(section = c("#2 Retail", "#1 Services", "#4 Restaurant", "#3 Foods"), 
-                            Proportion = c(275, 300, 50, 150))
-        
-        
-        
-    })
+    mall1 <- data.frame(section = c("#1 Retail", "#2 Services", "#3 Restaurant", "#4 Foods"), 
+                        Proportion = c(200, 175, 150, 50))
     
-    b1 <- reactive({
-        
-        b1 <- data.frame(section = c("#1 - Zara", "#2 - H&M", "#3 - J.Crew", "#4 - Forever 21","#5 - Urban Outfitter"), 
-                            Proportion = c(500, 300, 275, 150,50))
-        
-        
-        
-    })
+    
+    
+  })
   
+  mall2 <- reactive({
+    
+    mall2 <- data.frame(section = c("#2 Retail", "#1 Services", "#4 Restaurant", "#3 Foods"), 
+                        Proportion = c(275, 300, 50, 150))
+    
+    
+    
+  })
   
+  b1 <- reactive({
+    
+    b1 <- data.frame(section = c("#1 - Zara", "#2 - H&M", "#3 - J.Crew", "#4 - Forever 21","#5 - Urban Outfitter"), 
+                     Proportion = c(500, 300, 275, 150,50))
+    
+    
+    
+  })
   
   tags$div(id ="Rectangle",output$M1 <- renderPlot({ 
     
@@ -85,61 +92,76 @@ server = function(input, output) {
   })  
   
   
-    output$Mall1 <- renderPlot({ 
-        
-        ggplot(mall1(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
-            geom_bar(stat = "identity",width = 0.9) +coord_flip()+
-            scale_fill_brewer(palette = "Set2") +
-            theme_minimal(base_size = 16) +
-            ylab(NULL) +
-            xlab(NULL) + 
-            theme(legend.position = "none", panel.grid = element_blank(), 
-                  axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
-            scale_fill_manual(values= c("#ab1818","#cc4b14","#e97809", "#ffa600")) +ylim(0,300)
-    })  
+  output$M3 <- renderPlot({ 
     
-    output$Mall2<- renderPlot({ 
-        
-        ggplot(mall2(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
-            geom_bar(stat = "identity",width = 0.9) +coord_flip()+
-            scale_fill_brewer(palette = "Set2") +
-            theme_minimal(base_size = 16) +
-            ylab(NULL) +
-            xlab(NULL) + 
-            theme(legend.position = "none", panel.grid = element_blank(), 
-                  axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
-            scale_fill_manual(values= c("#ab1818","#cc4b14","#e97809", "#ffa600"))+ylim(0,300)
-    })  
+    ggplot(mall(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
+      geom_bar(stat = "identity",width = 0.9) +coord_flip()+
+      scale_fill_brewer(palette = "Set2") +
+      theme_minimal(base_size = 16) +
+      ylab(NULL) +
+      xlab(NULL) + 
+      theme(legend.position = "none", panel.grid = element_blank(), 
+            axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
+      scale_fill_manual(values= c("#cc4b14","#e27c4f","#f3ab89", "#ffd9c7"))
+  })  
+  observeEvent(input$dropb,
+               output$mytable <- DT::renderDataTable({a()}))
+  
+  output$Mall1 <- renderPlot({ 
     
-    output$m1_1 <- renderText({
-        "East York Mall"
-    })
+    ggplot(mall1(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
+      geom_bar(stat = "identity",width = 0.9) +coord_flip()+
+      scale_fill_brewer(palette = "Set2") +
+      theme_minimal(base_size = 16) +
+      ylab(NULL) +
+      xlab(NULL) + 
+      theme(legend.position = "none", panel.grid = element_blank(), 
+            axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
+      scale_fill_manual(values= c("#ab1818","#cc4b14","#e97809", "#ffa600")) +ylim(0,300)
+  })  
+  
+  output$Mall2<- renderPlot({ 
     
-    output$m1_2 <- renderText({
-        paste("385 Noah Place Suit 878","Toronto, ON M6K 3P0",sep = "\n")
-    })
+    ggplot(mall2(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
+      geom_bar(stat = "identity",width = 0.9) +coord_flip()+
+      scale_fill_brewer(palette = "Set2") +
+      theme_minimal(base_size = 16) +
+      ylab(NULL) +
+      xlab(NULL) + 
+      theme(legend.position = "none", panel.grid = element_blank(), 
+            axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
+      scale_fill_manual(values= c("#ab1818","#cc4b14","#e97809", "#ffa600"))+ylim(0,300)
+  })  
+  
+  output$m1_1 <- renderText({
+    "East York Mall"
+  })
+  
+  output$m1_2 <- renderText({
+    paste("385 Noah Place Suit 878","Toronto, ON M6K 3P0",sep = "\n")
+  })
+  
+  output$m2_1 <- renderText({
+    "Cloverdale Mall"
+  })
+  
+  output$m2_2 <- renderText({
+    paste("250 The East Mall","Etobicoke, ON M9B 3Y80",sep = "\n")
+  })
+  
+  
+  
+  output$b1<- renderPlot({ 
     
-    output$m2_1 <- renderText({
-        "Cloverdale Mall"
-    })
-    
-    output$m2_2 <- renderText({
-        paste("250 The East Mall","Etobicoke, ON M9B 3Y80",sep = "\n")
-    })
-    
-    
-    
-    output$b1<- renderPlot({ 
-        
-        ggplot(b1(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
-            geom_bar(stat = "identity",width = 0.9) +coord_flip()+
-            scale_fill_brewer(palette = "Set2") +
-            theme_minimal(base_size = 16) +
-            ylab(NULL) +
-            xlab(NULL) + 
-            theme(legend.position = "none", panel.grid = element_blank(), 
-                  axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
-            scale_fill_manual(values= c(alpha(rgb(146/255,120/255,194/255),0.48),alpha(rgb(191/255,114/255,189/255),0.48),alpha(rgb(232/255,107/255,171/255),0.48),
-                                        alpha(rgb(255/255,105/255,142/255),0.48),alpha(rgb(255/255,115/255,106/255),0.48)))+ylim(0,500)
-    })  
+    ggplot(b1(), aes(x = reorder(section, Proportion), y = Proportion,fill =section)) +
+      geom_bar(stat = "identity",width = 0.9) +coord_flip()+
+      scale_fill_brewer(palette = "Set2") +
+      theme_minimal(base_size = 16) +
+      ylab(NULL) +
+      xlab(NULL) + 
+      theme(legend.position = "none", panel.grid = element_blank(), 
+            axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +
+      scale_fill_manual(values= c(alpha(rgb(146/255,120/255,194/255),0.48),alpha(rgb(191/255,114/255,189/255),0.48),alpha(rgb(232/255,107/255,171/255),0.48),
+                                  alpha(rgb(255/255,105/255,142/255),0.48),alpha(rgb(255/255,115/255,106/255),0.48)))+ylim(0,500)
+  })  
 }
--- ui.R
+++ ui.R
@@ -3,69 +3,81 @@ library(shinyWidgets)
 library(shinydashboard)
 library(ggplot2)
 library(leaflet)
+library(DT)
+library(tidyverse)
 
-dataset <- diamonds
 
 ui <- fluidPage(
     title ="Retail Intelligence",
-   
+    
     tags$div(id="outer",
              h2("X-MART"),
-        tags$head(
-            # Include our custom CSS
-            includeCSS("C:/Users/hkumar062/Documents/New folder/X-MART/www/styles.css")
-        
-        ),
-        sidebarLayout(
-            
-            
-            sidebarPanel(
-                h4(class ='Location-Input',"Location Input"),
-               tags$div(class="line", br()),
-               setSliderColor("#d55404",c(1)),
-               selectInput("inputId", "Location Type",   list('Multiple','Single'), selected = NULL, multiple = FALSE,
-                           selectize = TRUE, width = NULL, size = NULL),
-               conditionalPanel("input.inputId == 'Multiple'",
-                                # Only prompt for threshold when coloring or sizing by superzip
-                                fileInput('datafile', 'Choose CSV file',
-                                          accept=c('text/csv', 'text/comma-separated-values,text/plain'))
-              
-               ),
-               conditionalPanel("input.inputId == 'Single'",
-                                # Only prompt for threshold when coloring or sizing by superzip
-                                textInput("Place","Enter the complete address of your retail property")),
-              # chooseSliderSkin("Orange"),
-                tags$div(sliderInput("Radius","Radius",min=0,max=20,step=0.5,value=4)),
-                actionButton("button",tags$div(id ="Button-Background",tags$div(id="White", tags$em(id ="Values","Find Location")),color="White"))
-              
-            ),
-            
-            mainPanel(
-                leaflet()
-            )),
-       # hr(),
-    fluidRow( column=4,
-        tabsetPanel(
-            tabPanel("Segment",tags$div(id ="Locations-broken-dow",tags$h5(tags$em("Location broken down by segmentation"))),
-                    tags$div(id="outer",
-                         # A static valueBox
-                        plotOutput("M1",height = "120px"),
-                        plotOutput("M2",height = "120px")
-                       # plotOutput("M3",height = "400px")
-                     )
-                     ), 
-            tabPanel("Location", verbatimTextOutput("summary"),fluidRow(style = "border-bottom: 1px solid rgba(0, 0, 0, .2);",
-                               column(3, offset = 1, br(), br(),br(),
-                                      textOutput("m1_1"),tags$head(tags$style("#m1_1{font-weight: bold; font-size: 120%; text-align: left; vertical-align: middle;}")),
-                                      textOutput("m1_2"),tags$head(tags$style("#m1_2{font-size: 100%; opacity: 0.75; text-align: left; vertical-align: top;}"))),
-                               column(8,tags$h4("Top Retail Category Opportunity",align = "left"), plotOutput(outputId = "Mall1", height = "200px"))),
-                               fluidRow(style = "border_bottom: 1px solid rgba(0, 0, 0, .2);",
-                                   column(3, offset = 1, br(),
-                                          textOutput("m2_1"),tags$head(tags$style("#m2_1{font-weight: bold;font-size: 120%; text-align: left; vertical-align: middle;}")),
-                                          textOutput("m2_2"),tags$head(tags$style("#m2_2{font-size: 100%; opacity: 0.75; text-align: left; vertical-align: top;}"))),
-                                   column(8, plotOutput(outputId = "Mall2", height = "200px")))), 
-                          
-                            tabPanel("Category", tableOutput("table"),fluidRow(column(1),style = "border-bottom: 1px solid rgba(0, 0, 0, .2);",
-                             column(8,tags$h4("Brand Index",align = "left",opacity = 0.4), plotOutput(outputId = "b1", height = "200px"))))
-        )
-)))
+             tags$head(
+                 # Include our custom CSS
+                 includeCSS("C:/Users/hkumar062/Documents/New folder/X-MART/www/styles.css"),
+                 includeCSS("https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css")
+                 
+                 
+             ),
+             sidebarLayout(
+                 
+                 
+                 sidebarPanel(
+                     h4(class ='Location-Input',"Location Input"),
+                     tags$div(class="line", br()),
+                     setSliderColor("#d55404",c(1)),
+                     selectInput("inputId", "Location Type",   list('Multiple','Single'), selected = NULL, multiple = FALSE,
+                                 selectize = TRUE, width = NULL, size = NULL),
+                     conditionalPanel("input.inputId == 'Multiple'",
+                                      # Only prompt for threshold when coloring or sizing by superzip
+                                      fileInput('datafile', 'Choose CSV file',
+                                                accept=c('text/csv', 'text/comma-separated-values,text/plain'))
+                                      
+                     ),
+                     conditionalPanel("input.inputId == 'Single'",
+                                      # Only prompt for threshold when coloring or sizing by superzip
+                                      textInput("Place","Enter the complete address of your retail property")),
+                     # chooseSliderSkin("Orange"),
+                     tags$div(sliderInput("Radius","Radius",min=0,max=20,step=0.5,value=4)),
+                     actionButton("button",tags$div(id ="Button-Background",tags$div(id="White", tags$em(id ="Values","Find Location")),color="White"))
+                     
+                 ),
+                 
+                 mainPanel(
+                     leaflet()
+                 )),
+             # hr(),
+             fluidRow( column=4,
+                       tabsetPanel(
+                           tabPanel("Segment",tags$div(id ="Locations-broken-dow",tags$h5(tags$em("Location broken down by segmentation"))),
+                                    tags$div(id="outer",
+                                             # A static valueBox
+                                             plotOutput("M1",height = "120px"),
+                                             tags$div (class="expand_more_24px",
+                                                       actionButton("dropb",class="expand_more_24px", type="button" ,
+                                                                    tags$span(class="caret"))),
+                                             tags$div( class ="dt",DT::dataTableOutput("mytable"))),
+                                    tags$div(id="outer",
+                                             plotOutput("M2",height = "120px"),
+                                             tags$div (class="expand_more_24px",
+                                                       tags$button (class="expand_more_24px", type="button" ,
+                                                                    tags$span(class="caret")))
+                                             # plotOutput("M3",height = "400px")
+                                    )
+                           ), 
+                           tabPanel("Location", verbatimTextOutput("summary"),fluidRow(style = "border-bottom: 1px solid rgba(0, 0, 0, .2);",
+                                                                                       column(3, offset = 1, br(), br(),br(),
+                                                                                              textOutput("m1_1"),tags$head(tags$style("#m1_1{font-weight: bold; font-size: 120%; text-align: left; vertical-align: middle;}")),
+                                                                                              textOutput("m1_2"),tags$head(tags$style("#m1_2{font-size: 100%; opacity: 0.75; text-align: left; vertical-align: top;}"))),
+                                                                                       column(8,tags$h4("Top Retail Category Opportunity",align = "left"), plotOutput(outputId = "Mall1", height = "200px"))),
+                                    fluidRow(style = "border_bottom: 1px solid rgba(0, 0, 0, .2);",
+                                             column(3, offset = 1, br(),
+                                                    textOutput("m2_1"),tags$head(tags$style("#m2_1{font-weight: bold;font-size: 120%; text-align: left; vertical-align: middle;}")),
+                                                    textOutput("m2_2"),tags$head(tags$style("#m2_2{font-size: 100%; opacity: 0.75; text-align: left; vertical-align: top;}"))),
+                                             column(8, plotOutput(outputId = "Mall2", height = "200px")))), 
+                           
+                           tabPanel("Category", tableOutput("table"),fluidRow(column(1),style = "border-bottom: 1px solid rgba(0, 0, 0, .2);",
+                                                                              column(8,tags$h4("Brand Index",align = "left",opacity = 0.4), plotOutput(outputId = "b1", height = "200px"))))
+                       )
+                       
+             )))
