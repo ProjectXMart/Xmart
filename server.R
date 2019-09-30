3a96b71320f20b78e9dd1d4964d0ad44d411aede
amberlifengyan <55359356+amberlifengyan@users.noreply.github.com>
2019-09-29 22:16:38 -0400

    Update server.R
    Updated Segmentation Structure

--- server.R
+++ server.R
@@ -1,7 +1,7 @@
 library(googleway)
 library(geosphere)
 library(ggmap)
-server <- function(input, output) {
+server <- function(input, output,session) {
   observeEvent(input$button,{
   
   ######Single Option Trade Area Calculation####
@@ -56,17 +56,27 @@ server <- function(input, output) {
   }
   )
   
-  tradearea = read.csv("C:/Users/ali518/Desktop/X-Mart/Trial1/tradearea_new.csv")
+ tradearea = read.csv("C:/Users/ali518/Desktop/X-Mart/Trial1/tradearea_new.csv")
      tradearea = tradearea$PostCode
 
     
 
-        persona = read.csv("C:/Users/ali518/Desktop/X-Mart/Trial1/persona.csv")
-        persona = persona %>% filter(PostCode %in% tradearea)
+        p = read.csv("C:/Users/ali518/Desktop/X-Mart/Trial1/persona.csv")
+        persona = p %>% filter(PostCode %in% tradearea)
         persona = count(persona, Segmentation)
-        persona$n = round(persona$n*100/sum(persona$n),2)
+        persona$n = round(persona$n*100/sum(persona$n),0)
+        persona = persona[persona$n != 0, ]
         persona <- persona %>%
           mutate(item = "Persona")
+        
+        
+        test= readRDS("C:/Users/ali518/Desktop/X-Mart/table.rds")
+        test = test %>% filter(PostCode %in% tradearea)
+        test = test %>% filter(Segmentation %in% persona$Segmentation)
+        test = test %>% group_by(Segmentation) %>% summarise_if(is.numeric,mean)
+        test = t(test)
+       
+        
 
 
      
@@ -74,7 +84,8 @@ server <- function(input, output) {
       affluence = read.csv("C:/Users/ali518/Desktop/X-Mart/Trial1/affluence.csv")
       affluence = affluence %>% filter(PostCode %in% tradearea)
       affluence = count(affluence, Segmentation)
-      affluence$n = round(affluence$n*100/sum(affluence$n),2)
+      affluence$n = round(affluence$n*100/sum(affluence$n),0)
+      affluence = affluence[affluence$n != 0, ]
       affluence <- affluence %>% 
         mutate(item = "Affluence")
         
@@ -86,7 +97,8 @@ server <- function(input, output) {
       behaviour = read.csv("C:/Users/ali518/Desktop/X-Mart/Trial1/behaviour.csv")
       behaviour = behaviour %>% filter(PostCode %in% tradearea)
       behaviour = count(behaviour, Segmentation)
-      behaviour$n = round(behaviour$n*100/sum(behaviour$n),2)
+      behaviour$n = round(behaviour$n*100/sum(behaviour$n),0)
+      behaviour = behaviour[behaviour$n != 0, ]
       behaviour <- behaviour %>% 
         mutate(item = "Behaviour")
       
@@ -127,15 +139,15 @@ server <- function(input, output) {
       # b1 = b1[order(-b1$V1),]
       # Books = b1
       # 
-      # b1 = readRDS("C:/Users/ali518/Desktop/X-Mart/Trial1/Cloth.rds")
-      # b1 = b1 %>% filter(PostCode %in% tradearea)
-      # b1 = apply(b1[,3:ncol(b1)], 2, mean)
-      # b1 = as.matrix(b1)
-      # b1 = as.data.frame(b1)
-      # b1 <- b1 %>%
-      #   mutate(Brand = rownames(b1))
-      # b1 = b1[order(-b1$V1),]
-      # Clothing = b1
+      b1 = readRDS("C:/Users/ali518/Desktop/X-Mart/Trial1/Cloth.rds")
+      b1 = b1 %>% filter(PostCode %in% tradearea)
+      b1 = apply(b1[,3:ncol(b1)], 2, mean)
+      b1 = as.matrix(b1)
+      b1 = as.data.frame(b1)
+      b1 <- b1 %>%
+        mutate(Brand = rownames(b1))
+      b1 = b1[order(-b1$V1),]
+      Clothing = b1
       # 
       # b1 = readRDS("C:/Users/ali518/Desktop/X-Mart/Trial1/HI.rds")
       # b1 = b1 %>% filter(PostCode %in% tradearea)
@@ -222,68 +234,87 @@ server <- function(input, output) {
       # 
   
     
-    #Render Mall category Breakdown
-    output$Categories <- renderUI({
-        lapply(1:1, function(i){
-            
-            fluidRow(style = "border-bottom: 1px solid rgba(0, 0, 0, .2);",
-                     column(3, offset = 1, br(), br(),br(),
-                            textOutput("m1_1"),tags$head(tags$style("#m1_1{font-weight: bold; font-size: 120%; text-align: left; vertical-align: middle;}")),
-                            htmlOutput("m1_2"),tags$head(tags$style("#m1_2{font-size: 100%; opacity: 0.75; text-align: left; vertical-align: top;}")),actionButton("go","View Details")),
-                      
-                     column(8,tags$h4("Top Retail Category Opportunity",align = "left"), uiOutput("c1")))
-            
-        })
-    })
     
     output$Details <- renderUI({
       bsModal("modalExample", "Details", trigger = "", size = "large",
+              
+              fluidRow(column(1,br(),renderText("Persona")),
+                       
+                       column(1,align = "center",offset = 0,useShinyjs(),div(style = "margin-top: 12px; margin-right: 0px",actionButton("p",class="expand_more_10px", type="button" ,
+                tags$span(class="caret")))),column(10,offset = 0,align = "left",style='padding:0px;',
+                      
               renderPlot(ggplot(persona, aes(x = item, y = n, fill = forcats::fct_rev(Segmentation),order = n)) +
                            geom_col() +
                            geom_text(aes(label = paste(n, "%")),
                                      position = position_stack(vjust = 0.5)) +coord_flip()+
-                           scale_fill_brewer(palette = "Set2") +
                            theme_minimal(base_size = 16) +
                            ylab(NULL) +
                            xlab(NULL) + 
                            theme(legend.position = "bottom",panel.grid = element_blank(), 
-                                 axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x=element_blank(),legend.title = element_blank())+
-                           scale_fill_manual(values= c("#ffd9c7","#f3ab89","#e27c4f","#cc4b14"))+guides(fill = guide_legend(reverse = TRUE)),height = 120),
+                                 axis.title = element_blank(),axis.ticks.x = element_blank(),axis.text=element_blank(),legend.title = element_blank())+
+                           scale_fill_manual(values= c("#ffd9c7","#f3ab89","#e27c4f","#cc4b14"))+
+                           guides(fill = guide_legend(reverse = TRUE)),height = 120),hidden(tableOutput("table_persona")))),
+              
+              fluidRow(column(1,br(),renderText("Affluence")),
+                       
+                       column(1,align = "center",offset = 0,useShinyjs(),div(style = "margin-top: 12px; margin-right: 0px",actionButton("a",class="expand_more_10px", type="button" ,
+                                                                                                                                        tags$span(class="caret")))),column(10,offset = 0,align = "left",style='padding:0px;',
+                                                                                                                                                                           
+                                                                                                                                                                           renderPlot(ggplot(affluence, aes(x = item, y = n, fill = forcats::fct_rev(Segmentation),order = n)) +
+                                                                                                                                                                                        geom_col() +
+                                                                                                                                                                                        geom_text(aes(label = paste(n, "%")),
+                                                                                                                                                                                                  position = position_stack(vjust = 0.5)) +coord_flip()+
+                                                                                                                                                                                        theme_minimal(base_size = 16) +
+                                                                                                                                                                                        ylab(NULL) +
+                                                                                                                                                                                        xlab(NULL) + 
+                                                                                                                                                                                        theme(legend.position = "bottom",panel.grid = element_blank(), 
+                                                                                                                                                                                              axis.title = element_blank(),axis.ticks.x = element_blank(),axis.text=element_blank(),legend.title = element_blank())+
+                                                                                                                                                                                        scale_fill_manual(values= c("#ffd9c7","#f3ab89","#e27c4f","#cc4b14"))+
+                                                                                                                                                                                        guides(fill = guide_legend(reverse = TRUE)),height = 120),hidden(tableOutput("table_affluence")))),
+              fluidRow(column(1,br(),renderText("Behaviour")),
+                       
+                       column(1,align = "center",offset = 0,useShinyjs(),div(style = "margin-top: 12px; margin-right: 0px",actionButton("b",class="expand_more_10px", type="button" ,
+                                                                                                                                        tags$span(class="caret")))),column(10,offset = 0,align = "left",style='padding:0px;',
+                                                                                                                                                                           
+                                                                                                                                                                           renderPlot(ggplot(behaviour, aes(x = item, y = n, fill = forcats::fct_rev(Segmentation),order = n)) +
+                                                                                                                                                                                        geom_col() +
+                                                                                                                                                                                        geom_text(aes(label = paste(n, "%")),
+                                                                                                                                                                                                  position = position_stack(vjust = 0.5)) +coord_flip()+
+                                                                                                                                                                                        theme_minimal(base_size = 16) +
+                                                                                                                                                                                        ylab(NULL) +
+                                                                                                                                                                                        xlab(NULL) + 
+                                                                                                                                                                                        theme(legend.position = "bottom",panel.grid = element_blank(), 
+                                                                                                                                                                                              axis.title = element_blank(),axis.ticks.x = element_blank(),axis.text=element_blank(),legend.title = element_blank())+
+                                                                                                                                                                                        scale_fill_manual(values= c("#ffd9c7","#f3ab89","#e27c4f","#cc4b14"))+
+                                                                                                                                                                                        guides(fill = guide_legend(reverse = TRUE)),height = 120),hidden(tableOutput("table_behaviour"))))
+                       
+                       )
+                  
               
               
-              
-              renderPlot( ggplot(affluence, aes(x = item, y = n, fill = forcats::fct_rev(Segmentation),order = n)) +
-                                                                                    geom_col() +
-                                                                                    geom_text(aes(label = paste(n, "%")),
-                                                                                              position = position_stack(vjust = 0.5)) +coord_flip()+
-                                                                                    scale_fill_brewer(palette = "Set2") +
-                                                                                    theme_minimal(base_size = 16) +
-                                                                                    ylab(NULL) +
-                                                                                    xlab(NULL) + 
-                                                                                    theme(legend.position = "bottom",panel.grid = element_blank(), 
-                                                                                          axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),legend.title = element_blank())+
-                                                                                    scale_fill_manual(values= c("#ffdbc2","#fdba89","#f59951","#e97809")) + guides(fill = guide_legend(reverse = TRUE)),height = 120),
-      
-    renderPlot( ggplot(behaviour, aes(x = item, y = n, fill = forcats::fct_rev(Segmentation),order = n)) +
-                  geom_col() +
-                  geom_text(aes(label = paste(n, "%")),
-                            position = position_stack(vjust = 0.5)) +coord_flip()+
-                  scale_fill_brewer(palette = "Set2") +
-                  theme_minimal(base_size = 16) +
-                  ylab(NULL) +
-                  xlab(NULL) + 
-                  theme(legend.position = "bottom",panel.grid = element_blank(), 
-                        axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),legend.title = element_blank())+
-                  scale_fill_manual(values= c("#ffdbc2","#fdba89","#f59951","#e97809")) + guides(fill = guide_legend(reverse = TRUE))
-    ,height = 120) ) 
-      
-      
       
     })
     
+    output$Allocation <- renderUI({
+      bsModal("sq_all", "Optimal Space Allocation", trigger = "", size = "large")
+    })
     
-    observeEvent(input$go, {
+    
+    output$table_persona <- renderTable({
+      test
+    }, rownames = TRUE, colnames = FALSE)
+    
+    
+    observeEvent(input$seg, {
       toggleModal(session, "modalExample", "open")
+    })
+    
+    observeEvent(input$p, {
+      toggle("table_persona")
+    })
+    
+    observeEvent(input$alloc, {
+      toggleModal(session,"sq_all","open")
     })
 
     
@@ -295,12 +326,27 @@ server <- function(input, output) {
         HTML(paste("385 Noah Place Suit 878","Toronto, ON M6K 3P0",sep = "<br/>"))
     })
     
+    #Render Mall category Breakdown
+    output$Categories <- renderUI({
+      lapply(1:1, function(i){
+        
+        fluidRow(style = "border-bottom: 1px solid rgba(0, 0, 0, .2);",
+                 column(3, offset = 1, br(), br(),br(),
+                        textOutput("m1_1"),tags$head(tags$style("#m1_1{font-weight: bold; font-size: 180%; text-align: left; vertical-align: middle;}")),
+                        htmlOutput("m1_2"),tags$head(tags$style("#m1_2{font-size: 150%; opacity: 0.75; text-align: left; vertical-align: top;}")),
+                        actionButton("seg","Segmentation",style = "background-color: white; color: chocolate; text-decoration: underline; border-style: none;font-size: 120%;"),
+                        actionButton("alloc","Recommended Space Allocation",style = "background-color: white; color: chocolate; text-decoration: underline; border-style: none;font-size: 120%;")),
+                 
+                 column(8,tags$h4("Top Retail Category Opportunity",align = "left"), uiOutput("c1")))
+        
+      })
+    })
     
     
     #fill color
-    color = c("#ab1818","#cc4b14","#e97809", "#ffa600","#ab1818","#cc4b14","#e97809", "#ffa600","#ab1818","#cc4b14","#e97809", "#ffa600")
+    color = rev(c(brewer.pal(9,"YlOrRd"),brewer.pal(9,"Oranges")))
     
-    
+    brand_color = c(brewer.pal(11,"PRGn"),brewer.pal(9,"Purples"),brewer.pal(9,"Blues"))
     
     #mall 1 should be pre-ordered
     output$c1 <- renderUI({
@@ -323,10 +369,7 @@ server <- function(input, output) {
       
     })
     
-    # observeEvent(input$hideshow1, {
-    #     # every time the button is pressed, alternate between hiding and showing the plot
-    #     toggle(rank[1])
-    # })
+  
     
     
     lapply(1:nrow(category), function(i){
@@ -351,12 +394,26 @@ server <- function(input, output) {
         
         ggplot(Furniture, aes(x = reorder(Brand,V1), y = V1,fill =Brand)) +
             geom_bar(stat = "identity",width = 0.9) +coord_flip()+
-            scale_fill_brewer(palette = "Set2") +
+        scale_fill_manual(values = alpha(brand_color,alpha = 0.48)) +
             theme_minimal(base_size = 16) +
             ylab(NULL) +
             xlab(NULL) + 
             theme(legend.position = "none", panel.grid = element_blank(), 
-                  axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +ylim(0,max(Furniture$V1))+scale_fill_brewer(palette="Spectral")
+                  axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +ylim(0,max(Furniture$V1))
 
+    })  
+    
+    
+    output$Clothing<- renderPlot({ 
+      
+      ggplot(Clothing, aes(x = reorder(Brand,V1), y = V1,fill =Brand)) +
+        geom_bar(stat = "identity",width = 0.9) +coord_flip()+
+        scale_fill_manual(values = alpha(brand_color,alpha = 0.48)) +
+        theme_minimal(base_size = 16) +
+        ylab(NULL) +
+        xlab(NULL) + 
+        theme(legend.position = "none", panel.grid = element_blank(), 
+              axis.title.x = element_blank(),axis.ticks.x = element_blank(),axis.text.x = element_blank(),axis.text.y = element_text(hjust = 0)) +ylim(0,max(Clothing$V1))
+      
     })  
 }
